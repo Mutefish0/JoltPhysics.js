3e3b5ff0bae3db323395d72fe1ae1ca693dedc17
Jorrit Rouwe <jrouwe@gmail.com>
2026-08-23 16:14:57 +0200

    Removed replace_by_import workaround
    See: https://github.com/emscripten-core/emscripten/issues/24050

--- multi-threaded.js
+++ multi-threaded.js
@@ -3,9 +3,7 @@ if (ENVIRONMENT_IS_PTHREAD) {
 	const _invokeEntryPoint = invokeEntryPoint;
 	invokeEntryPoint = (ptr, arg) => {
 		if (arg.script) {
-			// Workaround for bug: https://github.com/jrouwe/JoltPhysics.js/issues/245
-			// `replace_by_import` gets replaced by `import`
-			replace_by_import(arg.script).then(module => {
+			import(arg.script).then(module => {
 				module.default(Module, arg.params).then(() => _invokeEntryPoint(ptr, arg.value));
 			})
 		} else {
